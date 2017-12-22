822410f50e36af6775ad4fd84359b22dfb4a8e69
schandreas <andreas@restental.at>
2017-12-22 19:02:02 +0100

    Adding Docu in Distance Sensor and removing a leftover IOException

--- rpi_fs_pwm/src/at/restental/andreas/distance_sensor/DistanceSensor.java
+++ rpi_fs_pwm/src/at/restental/andreas/distance_sensor/DistanceSensor.java
@@ -8,11 +8,27 @@ import java.nio.file.Path;
 public class DistanceSensor {
 	protected Path measure;
 
-	public DistanceSensor(int pin1, int pin2, int timeout) throws IOException {
+	/**
+	 * Constructor for Distance Sensor. Resolves the directory in which the
+	 * specified sensor files will sit
+	 * 
+	 * @param pin1
+	 *            sensor pin 1
+	 * @param pin2
+	 *            sensor pin 2
+	 * @param timeout
+	 *            timeout for the sensor
+	 */
+	public DistanceSensor(int pin1, int pin2, int timeout) {
 		Path sensor_root = FileSystems.getDefault().getPath("/sys/class/distance-sensor/");
 		this.measure = sensor_root.resolve("distance_" + pin1 + "_" + pin2 + "/measure");
 	}
 
+	/**
+	 * gets the distance out of the measure file
+	 * 
+	 * @return the distance the sensor detects
+	 */
 	public int getDistance() {
 		try {
 			return new Integer(new String(Files.readAllBytes(measure)));
